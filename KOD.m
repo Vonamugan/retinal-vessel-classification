28ea12830b7f4d4e140f8d989a7bf3411c637095
YKamar <100069291+YKamar@users.noreply.github.com>
2022-03-16 19:01:19 +0100

    Update KOD.m

--- KOD.m
+++ KOD.m
@@ -1,6 +1,7 @@
 clear all
 clc
 %% nacitani obr
+obr_rgb = imread("01_dr.JPG");
 obr = im2double(imread("01_dr.JPG"));
 obr_mask = im2double(imread("01_dr_mask.tif"));
 obr_bin = imread("01_dr.tif");
@@ -42,8 +43,8 @@ kriz2 = [0  0  0  1  0  0  0 ;
 
 
 M = zeros(size(skel));
-
-prah = 6.2;
+prah = 7;
+% prah = 6.2;
 for i = 1:size(skel, 1)-6      % ve for cyklu prochazime cely obrazek, vystrihneme ctverec 7x7 a provedeme konvoluci s krizem a krizem2
                                     % za predpokladem ze v miste uzlu soucet konvolucnich obrazku bude > nez vybrany prah  
     for j = 1:size(skel, 2)-6  
@@ -67,6 +68,52 @@ C = imfuse(M,skel);
 imshow(C);
 
 % vylepsit... do jednoho bodu???
+%% histogramy ruznych slozek
+R = im2double(obr_rgb(:,:,1));
+G = im2double(obr_rgb(:,:,2));
+B = im2double(obr_rgb(:,:,3));
+RG = im2double(R+G);
+RB = im2double(R+B);
+GB = im2double(B+G);
+RGB = im2double(R+G+B);
+
+figure(1)
+subplot(171)
+hodnoty_jen_cev = R(obr_bin);
+imhist(hodnoty_jen_cev);
+title("R");
+
+subplot(172)
+hodnoty_jen_cev = G(obr_bin);
+imhist(hodnoty_jen_cev);
+title("G");
+
+subplot(173)
+hodnoty_jen_cev = B(obr_bin) ;
+imhist(hodnoty_jen_cev);
+title("B");
+
+
+subplot(174)
+hodnoty_jen_cev = RG(obr_bin);
+imhist(hodnoty_jen_cev);
+title("R+G");
+
+subplot(175)
+hodnoty_jen_cev = GB(obr_bin);
+imhist(hodnoty_jen_cev);
+title("G+B (↓↓↓BEST↓↓↓)");
+
+
+subplot(176)
+hodnoty_jen_cev = RB(obr_bin) ;
+imhist(hodnoty_jen_cev);
+title("R+B");
+
+subplot(177)
+hodnoty_jen_cev = RGB(obr_bin) ;
+imhist(hodnoty_jen_cev);
+title("RGB");
 
 %% klasifikace cev mezi uzly 
 % ...
